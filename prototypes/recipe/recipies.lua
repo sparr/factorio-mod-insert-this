3aeca2fcd8f6be993f6fd18f0e0f9399e17e70de
Clarence Risher <sparr0@gmail.com>
2014-03-08 10:33:51 -0500

    disable recipes until technology researched

--- prototypes/recipe/recipies.lua
+++ prototypes/recipe/recipies.lua
@@ -3,6 +3,7 @@ data:extend(
   {
     type = "recipe",
     name = "inserter-90-left",
+    enabled = "false",
     ingredients =
     {
       {"basic-inserter", 1},
@@ -12,6 +13,7 @@ data:extend(
   {
     type = "recipe",
     name = "inserter-90-right",
+    enabled = "false",
     ingredients =
     {
       {"basic-inserter", 1},
@@ -21,6 +23,7 @@ data:extend(
   {
     type = "recipe",
     name = "inserter-near-pull",
+    enabled = "false",
     ingredients =
     {
       {"basic-inserter", 1},
@@ -30,6 +33,7 @@ data:extend(
   {
     type = "recipe",
     name = "inserter-near-push",
+    enabled = "false",
     ingredients =
     {
       {"basic-inserter", 1},
@@ -39,6 +43,7 @@ data:extend(
   {
     type = "recipe",
     name = "inserter-near-to-far",
+    enabled = "false",
     ingredients =
     {
       {"long-handed-inserter", 1},
@@ -48,12 +53,12 @@ data:extend(
   {
     type = "recipe",
     name = "inserter-far-to-near",
+    enabled = "false",
     ingredients =
     {
       {"long-handed-inserter", 1},
     },
     result = "inserter-far-to-near"
-  },
-  
+  },  
 }
 )
